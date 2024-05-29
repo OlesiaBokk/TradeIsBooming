2c1f3c3d45725b108278ed5e018172a766839892
OlesiaBokk <orbokk@gmail.com>
2024-05-29 19:06:25 +0300

    set current goods amount for Ship and Stock

--- src/main/java/cy/olesiabokk/tradeisboomingapp/main/Main.java
+++ src/main/java/cy/olesiabokk/tradeisboomingapp/main/Main.java
@@ -6,13 +6,15 @@ import cy.olesiabokk.tradeisboomingapp.util.Generator;
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
 
         Generator generator = new Generator();
 
         ArrayList<Berth> berthList = new ArrayList<>();
-        berthList.add(new Berth(new Stock(generator.addMaxCapacity(3999, 10001))));
-        berthList.add(new Berth(new Stock(generator.addMaxCapacity(3999, 10001))));
+        for (int i = 0; i < 1; i++) {
+            berthList.add(new Berth(new Stock(generator.addMaxCapacity(3999, 10001))));
+            berthList.get(i).setCurrStockAmount(generator.getRandomNum(4000, berthList.get(i).getMaxStockCapacity()));
+        }
         //berthList.add(new Berth(new Stock(generator.addMaxCapacity(3999, 10001))));
         //berthList.add(new Berth(new Stock(generator.addMaxCapacity(3999, 10001))));
 
@@ -24,8 +26,9 @@ public class Main {
         // запрашивать сколько места в берте в самом методе run
         // если есть место, выполняем джобку, если нет, освобождаем причал
 
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 2; i++) {
             Ship ship = new Ship(generator.addMaxCapacity(199, 501), generator.getRandomJob(), supervisor);
+            ship.setCurrentAmount(generator.getRandomNum(200, ship.getMaxCapacity()));
             Thread thread = new Thread(ship);
             thread.start();
         }
