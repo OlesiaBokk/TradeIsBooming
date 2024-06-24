c9577095329fb1b2b9b65705d6bf88d2c3a157e5
OlesiaBokk <orbokk@gmail.com>
2024-06-24 13:47:00 +0300

    little fix of alignment in Generator

--- src/main/java/cy/olesiabokk/tradeisboomingapp/util/Generator.java
+++ src/main/java/cy/olesiabokk/tradeisboomingapp/util/Generator.java
@@ -7,24 +7,24 @@ import java.util.Random;
 public class Generator {
     private final Random random = new Random();
 
-    public int getRandomNum(){
+    public int getRandomNum() {
         return random.nextInt();
     }
 
-    public int getRandomNum(int from, int to){
+    public int getRandomNum(int from, int to) {
         int randomNum = getRandomNum();
-        if(randomNum < from || randomNum > to){
+        if (randomNum < from || randomNum > to) {
             randomNum = (from + random.nextInt(to - from));
         }
         return randomNum;
     }
 
-    public int addMaxCapacity(int from, int to){
+    public int addMaxCapacity(int from, int to) {
         return getRandomNum(from, to);
     }
 
-    public JobType getRandomJob(){
+    public JobType getRandomJob() {
         JobType[] jobTypes = JobType.values();
-        return  jobTypes[random.nextInt(jobTypes.length)];
+        return jobTypes[random.nextInt(jobTypes.length)];
     }
 }
